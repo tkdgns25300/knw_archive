c8bc397eeb4d218743a21161ba1eae605735ffd7
shamshod <realshamshod01@gmail.com>
2022-04-05 09:58:06 +0900

    feat: get ProofItem with ChronologyInfo

--- src/repository/ProofItemQueryRepo.ts
+++ src/repository/ProofItemQueryRepo.ts
@@ -1,14 +1,35 @@
-import {EntityRepository} from "typeorm";
+import {createQueryBuilder, EntityRepository} from "typeorm";
 import { Service } from "typedi";
 
 import {ProofItem} from "../entity";
 import { BaseQueryRepo } from "./BaseQueryRepo";
+import { PageReq} from "../api";
 
 @Service()
 @EntityRepository(ProofItem)
 export class ProofItemQueryRepo extends BaseQueryRepo {
   constructor() {
     super('proof_item', 'ProofItem');
+  }
+
+  async findAll(param: PageReq) {
+    return await createQueryBuilder("proof_item")
+        .leftJoinAndSelect("ProofItem.chronology_id", "chronology_item")
+        .select([
+          "ProofItem.id",
+          "ProofItem.file_src",
+          "ProofItem.reference",
+          "ProofItem.content",
+          "ProofItem.is_visible",
+          "ProofItem.updated_at",
+          "ProofItem.created_at",
+          "chronology_item.id",
+          "chronology_item.content",
+          "chronology_item.period",
+        ])
+        .skip(param.getOffset())
+        .take(param.getLimit())
+        .getManyAndCount();
   }
 
 }
