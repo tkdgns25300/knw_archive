84f4dfd82d18f05378563bc157403680dd3112d7
shamshod <realshamshod01@gmail.com>
2022-04-08 11:18:09 +0900

    update: mainBanner sort by order

--- src/repository/MainBannerQueryRepo.ts
+++ src/repository/MainBannerQueryRepo.ts
@@ -1,14 +1,23 @@
-import { EntityRepository } from "typeorm";
+import {createQueryBuilder, EntityRepository} from "typeorm";
 import { Service } from "typedi";
 
 import { MainBanner } from "../entity";
 import { BaseQueryRepo } from "./BaseQueryRepo";
+import {PageReq} from "../api";
 
 @Service()
 @EntityRepository(MainBanner)
 export class MainBannerQueryRepo extends BaseQueryRepo {
   constructor() {
     super('main_banner', 'MainBanner');
+  }
+
+  findAll(param: PageReq) {
+    return createQueryBuilder("main_banner")
+        .orderBy('MainBanner.order', param.getOrder)
+        .skip(param.getOffset())
+        .take(param.getLimit())
+        .getManyAndCount();
   }
 
 }
