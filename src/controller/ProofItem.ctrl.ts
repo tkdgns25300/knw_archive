4bb2fb04fdfea0c2fd24b91384fe811f684997c3
hun <tkdgns25300@gmail.com>
2022-08-22 13:26:55 +0900

    fix: 작가 연보 상세보기 수정

--- src/controller/ProofItem.ctrl.ts
+++ src/controller/ProofItem.ctrl.ts
@@ -13,6 +13,7 @@ import {checkAccessToken} from "../middlewares/AuthMiddleware";
 import {QueryFailedError} from "typeorm";
 import {logger} from "../util/logger";
 import {ProofItemDto} from "../dto";
+import { uploadFile } from "../util/fileUpload";
 
 @Service()
 @JsonController("/proof")
@@ -24,6 +25,9 @@ export class ProofItemController {
    @UseBefore(checkAccessToken)
     public async create(@Body({ options: { limit: "20mb" } }) createDto: ProofItemDto, @Res() res: Response) {
         try {
+            if (createDto.file_base64) {
+                await uploadFile(createDto.file_base64)
+            }
             return await this.proofItemService.create(createDto);
         } catch (err) {
             if (err instanceof QueryFailedError) {
